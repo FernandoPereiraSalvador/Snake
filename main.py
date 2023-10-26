997919931f7dc918eafbdb4f2423a64181e12bc1
FernandoPereiraSalvador <fernandopereirasalvador@gmail.com>
2023-10-26 13:47:12 +0200

    Menu
    Se ha añadido un menú antes de mostrar el tablero.

--- main.py
+++ main.py
@@ -1,7 +1,9 @@
 import pygame
-
 from constants import SCREEN_WIDTH, SCREEN_HEIGHT, CELL_SIZE
 
+# Definir colores
+BLACK = (0, 0, 0)
+WHITE = (255, 255, 255)
 
 def main():
     pygame.init()
@@ -9,14 +11,23 @@ def main():
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
     pygame.display.set_caption("Snake Game")
 
+    in_menu = True
+    events = []  # Lista para almacenar eventos
+
     while True:
-        for event in pygame.event.get():
+        events = pygame.event.get()  # Obtener eventos en cada iteración
+
+        for event in events:
             if event.type == pygame.QUIT:
                 pygame.quit()
                 return
-        draw(screen)
+            if in_menu:
+                start_game = menu(screen, events)  # Pasa la lista de eventos a la función del menú
+                if start_game:
+                    in_menu = False
+        if not in_menu:
+            draw(screen)
         pygame.display.update()
-
 
 def draw(screen):
     screen.fill((255, 255, 255))
@@ -28,6 +39,30 @@ def draw(screen):
             else:
                 pygame.draw.rect(screen, (255, 255, 255), (x, y, CELL_SIZE, CELL_SIZE))
 
+def menu(screen, events):
+    screen.fill((255, 255, 255))
+
+    # Dibuja el nombre del juego en el centro de la pantalla
+    font = pygame.font.Font(None, 36)
+    text = font.render("Snake Game", True, (0,0,0))
+    text_rect = text.get_rect()
+    text_rect.center = (SCREEN_WIDTH // 2, SCREEN_HEIGHT // 2 - 50)
+    screen.blit(text, text_rect)
+
+    # Dibuja un botón de inicio
+    button_rect = pygame.Rect(SCREEN_WIDTH // 2 - 100, SCREEN_HEIGHT // 2 + 50, 200, 50)
+    pygame.draw.rect(screen, (255,255,255), button_rect)
+    font = pygame.font.Font(None, 24)
+    button_text = font.render("Start Game", True, (0,0,0))
+    button_text_rect = button_text.get_rect()
+    button_text_rect.center = button_rect.center
+    screen.blit(button_text, button_text_rect)
+
+    for event in events:
+        if event.type == pygame.MOUSEBUTTONDOWN:
+            if button_rect.collidepoint(event.pos):
+                return True
+    return False
 
 if __name__ == "__main__":
     main()
