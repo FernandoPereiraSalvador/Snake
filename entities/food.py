f6e00764a4159915e2608aee56f42a05238b69ec
FernandoPereiraSalvador <fernandopereirasalvador@gmail.com>
2023-10-27 10:34:44 +0200

    Corrección
    Se ha corregido las manzanas para que no aparezcan en el marco

--- entities/food.py
+++ entities/food.py
@@ -4,9 +4,9 @@ from constants import SCREEN_WIDTH, CELL_SIZE, SCREEN_HEIGHT
 
 class Food:
     def __init__(self):
-        self.position = (random.randint(0, SCREEN_WIDTH // CELL_SIZE - 1) * CELL_SIZE,
-                        random.randint(0, SCREEN_HEIGHT // CELL_SIZE - 1) * CELL_SIZE)
+        self.randomize_position()
 
     def randomize_position(self):
-        self.position = (random.randint(0, SCREEN_WIDTH // CELL_SIZE - 1) * CELL_SIZE,
-                        random.randint(0, SCREEN_HEIGHT // CELL_SIZE - 1) * CELL_SIZE)
+        max_x = (SCREEN_WIDTH - 2 * CELL_SIZE) // CELL_SIZE  # Resta dos veces el tamaño de la celda
+        max_y = (SCREEN_HEIGHT - 2 * CELL_SIZE) // CELL_SIZE  # Resta dos veces el tamaño de la celda
+        self.position = (random.randint(1, max_x) * CELL_SIZE, random.randint(1, max_y) * CELL_SIZE)
